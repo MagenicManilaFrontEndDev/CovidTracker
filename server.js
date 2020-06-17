3ea501490bb53f300d5aabb5bc6149f3290b26d1
Mily Opeña <FlorencioO@magenic.com>
2020-06-17 09:03:12 +0800

    Added CORS support on APIs

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 
 connectDB();
 
+app.use(cors());
 app.use(express.json({ extended: false }));
 
 app.use('/api/visited-places', require('./routes/api/visited-places'));
